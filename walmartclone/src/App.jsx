25cbe36eafc02061611efb0700b8f6b3efcd4126
Mount-Blanc <mountblanc100@gmail.com>
2023-05-21 15:37:05 -0500

    added header component

--- walmartclone/src/App.jsx
+++ walmartclone/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header>
+      <div className='nav'>
+      <div>navleft
+        <div>logo</div>
+        <div>Departments</div>
+        <div>Services</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>Search</div>
+      <div>navrigh
+    <div>Reorder</div>
+    <div>sign in</div>
+    <div>Cart</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
+      <nav>
+        <div>How do you want your items?</div>
+      </nav>
+    </header>
     </>
   )
 }
