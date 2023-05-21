e0d168a5ffd0afe583c4569f42660bc812626892
Mount-Blanc <mountblanc100@gmail.com>
2023-05-21 15:46:40 -0500

    added the basics of the nav belt in header

--- walmartclone/src/App.jsx
+++ walmartclone/src/App.jsx
@@ -6,20 +6,42 @@ function App() {
     <>
     <header>
       <div className='nav'>
-      <div>navleft
-        <div>logo</div>
-        <div>Departments</div>
-        <div>Services</div>
+      <div className='nav-left'>navleft
+        <div className='nav-item'>logo</div>
+        <div className='nav-item'>Departments</div>
+        <div className='nav-item'>Services</div>
       </div>
       <div>Search</div>
-      <div>navrigh
-    <div>Reorder</div>
-    <div>sign in</div>
-    <div>Cart</div>
+      <div className='nav'>navrigh
+    <div className='nav-item'>Reorder</div>
+    <div className='nav-item'>sign in</div>
+    <div className='nav-item'>Cart</div>
       </div>
       </div>
-      <nav>
+      <nav className='nav'>
         <div>How do you want your items?</div>
+        <div>location</div>
+        <div>
+          <ul className='category-list'>
+            <li className='category-list-item'>
+              <a href=''>Deals</a>
+            </li>
+            <li className='category-list-item'>
+              <a href=''>Graduation</a>
+            </li> <li className='category-list-item'>
+              <a href=''>National Pet Month</a>
+            </li> <li className='category-list-item'>
+              <a href=''>Grocery & essentials</a>
+            </li> <li className='category-list-item'>
+              <a href=''>Fashion</a>
+            </li>
+            <li className='category-list-item'>
+              <a href=''>Home</a>
+            </li> <li className='category-list-item'>
+              <a href=''>Toys</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
     </>
